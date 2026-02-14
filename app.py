15e885b0bb9998139eb28a5ab031790bee082ea0
rudeusredhat <rudeusredhat@gmail.com>
2026-02-15 01:52:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1499,7 +1499,7 @@ if __name__ == '__main__':
     print("")
     
     app.run(debug=True, host='0.0.0.0', port=5000)
-    import qrcode
+    
 
 # Your IP address
 ip = "192.168.43.123"  # Change to your IP
